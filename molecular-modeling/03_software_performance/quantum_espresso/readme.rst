cc6b634385208e9c1311b49ce55bff120d3f1d1a
MI <miro.ilias@gmail.com>
2026-08-21 16:13:46 +0300

    u

--- molecular-modeling/03_software_performance/quantum_espresso/readme.rst
+++ molecular-modeling/03_software_performance/quantum_espresso/readme.rst
@@ -2,5 +2,8 @@
 Quantum Espresso performance measurement
 ========================================
 
+Tl pseudopotential
+------------------
+https://www.pseudo-dojo.org/index.html
 
 
--- molecular-modeling/03_software_performance/quantum_espresso/tl_bulk_scf.py
+++ molecular-modeling/03_software_performance/quantum_espresso/tl_bulk_scf.py
@@ -1,5 +1,5 @@
 from ase.build import bulk
-from ase.calculators.espresso import Espresso
+from ase.calculators.espresso import Espresso, EspressoProfile
 import subprocess
 import os
 import sys
@@ -7,12 +7,6 @@ import sys
 def run_pw_parallel(calc, atoms, nproc=4, mpirun_cmd='mpirun'):
     """
     Run the Quantum ESPRESSO pw.x calculation in parallel.
-    
-    Parameters:
-    - calc: Espresso calculator object
-    - atoms: Atoms object
-    - nproc: Number of processors/cores to use
-    - mpirun_cmd: MPI launcher command (default: 'mpirun')
     """
     # First, write the input file
     calc.write_input(atoms)
@@ -22,7 +16,6 @@ def run_pw_parallel(calc, atoms, nproc=4, mpirun_cmd='mpirun'):
     output_file = os.path.join(calc.directory, 'pw.out')
     
     # Construct the parallel command
-    # Option 1: Using mpirun with -np flag
     cmd = f"{mpirun_cmd} -np {nproc} pw.x -i {input_file} > {output_file}"
     
     print(f"Running: {cmd}")
@@ -45,8 +38,9 @@ def run_pw_parallel(calc, atoms, nproc=4, mpirun_cmd='mpirun'):
 atoms = bulk('Tl', crystalstructure='hcp', a=3.46, c=5.52)
 
 # 2. Define the pseudopotentials
+# Make sure this file exists in your pseudopotential directory
 pseudopotentials = {
-    'Tl': 'Tl.rel-pbe-n-kjpaw_psl.1.0.0.UPF'
+    'Tl': 'Tl.upf'
 }
 
 # 3. Set input parameters for a two-component calculation
@@ -66,8 +60,16 @@ input_data = {
     },
 }
 
-# 4. Create the Espresso calculator
+# 4. Configure the Espresso calculator with the correct path
+# Using the path you found with 'which pw.x'
+pw_executable = '/home/milias/miniconda3/envs/molmatmodel/bin/pw.x'
+
+# Create an EspressoProfile with the executable path
+profile = EspressoProfile(command=pw_executable)
+
+# Create the Espresso calculator
 calc = Espresso(
+    profile=profile,           # <-- This provides the configuration
     directory='qe_tl_scf',
     input_data=input_data,
     pseudopotentials=pseudopotentials,
@@ -76,8 +78,8 @@ calc = Espresso(
 
 # 5. Run the calculation in parallel
 # Adjust the number of processors as needed
-NPROC = 8  # Change this to your desired number of processors
-MPI_CMD = 'mpirun'  # Use 'mpiexec' on some systems
+NPROC = 4  # You can increase this if you have more cores available
+MPI_CMD = 'mpirun'  # Use 'mpiexec' if 'mpirun' doesn't work
 
 run_pw_parallel(calc, atoms, nproc=NPROC, mpirun_cmd=MPI_CMD)
 
